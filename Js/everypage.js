87ede4b6d3b9a0f9741f7a4d3af9342bf702ddd2
T3chnx Studios <76597601+TechnoWolf7671@users.noreply.github.com>
2022-07-05 17:52:00 +0000

    V1

---
+++ Js/everypage.js
@@ -0,0 +1,19 @@
+function openNav() {
+  document.getElementById("menu").style.width = "30%";
+}
+
+function closeNav() {
+  document.getElementById("menu").style.width = "0%";
+}
+// function NewPage() {
+//   var body = document.getElementById("body")
+//   var menu = document.getElementById("menu");
+//   var menubutton = document.getElementById("openmenu");
+//   document.createElement(menu);
+//   document.createElement(menubutton);
+//   body.appendChild(menu)
+// }
+// window.onload = () => {  
+//   console.log("page is loaded");
+//   NewPage();
+// }
--- everypage.js
+++
@@ -1,7 +0,0 @@
-function openNav() {
-  document.getElementById("menu").style.width = "30%";
-}
-
-function closeNav() {
-  document.getElementById("menu").style.width = "0%";
-}
